1d70f073ba4536ac34051883935246f47f1f8959
caowei49 <88067457+caowei49@users.noreply.github.com>
2023-03-28 08:42:00 +0800

    Create parse_config.py

---
+++ yang/parse_config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+
+def get_config_data():
+    if os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), "../conf", "netconf.json"))):
+        conf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../conf", "netconf.json"))
+        with open(conf_path, 'r') as f:
+            try:
+                data = json.load(f)
+            except Exception as e:
+                sys.stderr.write(": Error : netconf.json format error !\n")
+        return data
+    else:
+        sys.stderr.write(": Error : netconf.json is not found !\n")
